c73fc900a522a5818f1724221c4a5aaa8c5a88ce
nino mal <nino_mrt@hotmail.com>
2023-10-12 18:25:16 -0300

    "Refact and fixing services"

--- services/service.py
+++ services/service.py
@@ -1,3 +1,13 @@
+ATIVO = "WINV23" #name of market
+VOLUME = 1.0 #volume observe compatibility volume error change for 1.0 Demo version
+ONDAS = 20
+MAGIC = 12345
+ONDAS_BOLINGER = 20
+STANDER_DEVIATIONS = 2
+TL_SD = 2
+SL_SD = 2
+BUY_SELL_STOP = 50
+DEVIATION = 0
 
 #Services
 class Service():
@@ -12,30 +22,20 @@ class Service():
         symbol_info_tick_dict = self.mt5.symbol_info_tick(self.SYMBOL)
         return symbol_info_tick_dict
     
-    ATIVO = "WINV23" #name of market
-    VOLUME = 1.0 #volume observe compatibility volume error change for 1.0 Demo version
-    ONDAS = 20
-    MAGIC = 10
-    ONDAS_BOLINGER = 20
-    STANDER_DEVIATIONS = 2
-    TL_SD = 2
-    SL_SD = 2
-    deviation = 0
-
     def buy(self):
-        preco = self.mt5.symbol_info_tick(self.ATIVO).ask
-        pontos = self.mt5.symbol_info(self.ATIVO).point
+        preco = self.mt5.symbol_info_tick(ATIVO).ask
+        pontos = self.mt5.symbol_info(ATIVO).point
         print(preco)
         request = ({
             "action": self.mt5.TRADE_ACTION_DEAL,
-            "symbol": self.ATIVO,
-            "volume": 1.0,
+            "symbol": ATIVO,
+            "volume": VOLUME,
             "type": self.mt5.ORDER_TYPE_BUY,
             "price": preco,
-            "deviation":self. deviation,
-            "sl": preco - 50 * pontos,
-            "tp":preco + 50 * pontos,
-            "magic": 12345,
+            "deviation":DEVIATION,
+            "sl": preco - BUY_SELL_STOP * pontos,
+            "tp":preco + BUY_SELL_STOP * pontos,
+            "magic": MAGIC,
             "comment": "python script buy",
             "type_time": self.mt5.ORDER_TIME_GTC,
             "type_filling": self.mt5.ORDER_FILLING_RETURN
@@ -45,20 +45,19 @@ class Service():
         self.buyOrders += 1
         print(result)
 
-
     def sell(self):
-        preco = self.mt5.symbol_info_tick(self.ATIVO).bid
-        pontos = self.mt5.symbol_info(self.ATIVO).point
+        preco = self.mt5.symbol_info_tick(ATIVO).bid
+        pontos = self.mt5.symbol_info(ATIVO).point
         request = ({
             "action": self.mt5.TRADE_ACTION_DEAL,
-            "symbol": self.ATIVO,
-            "volume": self.VOLUME,
+            "symbol": ATIVO,
+            "volume": VOLUME,
             "type": self.mt5.ORDER_TYPE_SELL,
             "price": preco,
-            "deviation": self.deviation,
-            "sl": preco + 50 * pontos,
-            "tp":preco - 50 * pontos,
-            "magic": 12345,
+            "deviation": DEVIATION,
+            "sl": preco + BUY_SELL_STOP * pontos,
+            "tp":preco - BUY_SELL_STOP * pontos,
+            "magic": MAGIC,
             "comment": "python script sell",
             "type_time": self.mt5.ORDER_TIME_GTC,
             "type_filling": self.mt5.ORDER_FILLING_RETURN
@@ -67,8 +66,7 @@ class Service():
         result = self.mt5.order_send(request)
         self.sellOrders += 1
         print(result)
-
-    
+  
     def lastick(self):
         #test
         lasttick= self.mt5.symbol()
@@ -78,10 +76,9 @@ class Service():
         symbol_info_tick_dict = lasttick._asdict()
         for prop in symbol_info_tick_dict:
             print("  {}={}".format(prop, symbol_info_tick_dict[prop]))
-              
-    
+                 
     def __repr__(self) -> str:    
         if(self.buyOrders != 0):
             return f"Buy {self.symbol}, {self.buyOrders} Ticks in market {self.SYMBOL}"
         elif(self.sellOrders != 0):
-            return f"Sell {self.symbol} , {self.sellOrders} TIicks in market {self.SYMBOL}"
+            return f"Sell {self.symbol}, {self.sellOrders} Ticks in market {self.SYMBOL}"
