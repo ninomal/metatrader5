57a8f49dc498c946c048ce02922dbc4f9ed6c1b5
nino mal <nino_mrt@hotmail.com>
2023-10-16 16:52:19 -0300

    add Products/ date colect

--- products/products.py
+++ products/products.py
@@ -1,25 +1,47 @@
 # implments volume in matplot basead in mt5 
-'''
+import pandas as pd
+import time
+from datetime import datetime
+import pytz
+import matplotlib as plt
+import pandas_ta as ta #calcule average and other function 
+
 ATIVO = "WINV23" #name of market
 POS = 0 # position bar
 COUNT = 0 #position count 
 
 class Products:
-    def __init__(self, mt5):
+    def __init__(self, mt5, date):
         self.COUNT = COUNT
         self.SYMBOL = ATIVO
         self.POS = POS
+        self.date = date
         self.mt5 = mt5     
-        self.dados = colectDate()
+        self.dados = self.colectDate()
+        self.TIMEFRAME = mt5.TIMEFRAME_M10
+    
+    def convertDateHour(self, df):
+        df['time'] = pd.to.datetime(df['time'], unit ='s')
+        return df
     
     def colectDate(self):
-        dateTime()
-        self.mt5.copy_rates_from_pos(SYMBOL, timeframe, self.POS, self.COUNT)
-        
+        date =self.mt5.copy_rates_from_pos(self.SYMBOL,self.TIMEFRAME, self.POS, self.COUNT)
+        date = pd.DateFrame(self.date)
+        date = self.convertDateHour(date)
+                
     def dateTime(self):
         named_tuple = time.localtime() 
         timeframe = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+        
+'''
+    def prev_Calc(self):
+        prev_calculated,_value = 
+        "iMA(NULL, 0, period, 0, MODE_SMA, PRICE_CLOSE, 0)"
+        
+    def rates_total(self):
+        bar = (start, )
 
+    
     def onCalculate(dados,
                     const int prev_calculated,
                     const datetime &time[], 
@@ -68,4 +90,37 @@ class Products:
         else
             ExtColorsBuffer[i]=1.0;
         }
-    //---'''
+    //---
+    
+    
+    
+    #test
+    void OnStart()
+  {
+//---
+   MqlRates rates[];
+   ArraySetAsSeries(rates,true);
+   int copied=CopyRates(Symbol(),0,0,100,rates);
+   if(copied>0)
+     {
+      Print("Bars copied: "+copied);
+      string format="open = %G, high = %G, low = %G, close = %G, volume = %d";
+      string out;
+      int size=fmin(copied,10);
+      for(int i=0;i<size;i++)
+        {
+         out=i+":"+TimeToString(rates[i].time);
+         out=out+" "+StringFormat(format,
+                                  rates[i].open,
+                                  rates[i].high,
+                                  rates[i].low,
+                                  rates[i].close,
+                                  rates[i].tick_volume);
+         Print(out);
+        }
+     }
+   else Print("Failed to get history data for the symbol ",Symbol());
+  }
+    
+  
+    '''
