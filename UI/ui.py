5d8934c32f0f54565006ba68846c588bfb98382a
nino mal <nino_mrt@hotmail.com>
2023-12-11 20:47:47 -0300

    implements converToDict need fix

--- UI/ui.py
+++ UI/ui.py
@@ -66,10 +66,18 @@ class UI(ProductsServices):
     def sortedRedBar(self):
         pvols = self.pvol
         times = self.time
+        time = str(times)
+        timess = int(time)
+        teste = self.convertToDict(pvols, timess)
+        print(teste)
         pvolSorted = sorted(pvols, reverse= True)
-        timeSorted = times #<-fix here
+        timeSorted = times 
         print(pvolSorted)
         print(timeSorted)
+        
+    def convertToDict(self, list, list2):
+        res_dct = map(lambda i: (list[i], list[list[i + list2]]), range(len(list)-1)[::2])
+        return dict(res_dct)
     
     @cache
     def lastGraph(self, switch):
